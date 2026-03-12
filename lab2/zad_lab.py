440cc0469f08cf21c6a6b87418f4e1fff6bf3581
Kacper Feliks <k.feliks@avsystem.com>
2026-03-12 22:51:10 +0100

    lab2

---
+++ lab2/zad_lab.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Dict
+
+
+# Task 1 – Doodle API example
+# • Create simple Doodle API
+# • Small API for voting
+# – User can create poll (see what is insider poll)
+# – User can cast a vote inside this polls
+# – User can add, update and delete all information
+# he provides
+# – User can see the results of votes
+# • Construct API and build the system
+# – Test it with the Swagger UI
+app = FastAPI()
+
+polls = {}
+votes = {}
+n=1
+class Poll(BaseModel):
+	question: str
+	options: List[str]
+
+class Vote(BaseModel):
+	option: str
+
+# Create poll
+@app.post("/polls")
+def create_poll(poll: Poll):
+	global n
+	poll_id = str(n+1)
+	polls[poll_id] = poll.model_dump()
+	votes[poll_id] = {}
+	n += 1 
+	return {"poll_id": poll_id}
+
+# Get poll
+@app.get("/polls/{poll_id}")
+def get_poll(poll_id: str):
+	return polls[poll_id]
+
+# Update poll
+@app.put("/polls/{poll_id}")
+def update_poll(poll_id: str, poll: Poll):
+	polls[poll_id] = poll.model_dump()
+	return polls[poll_id]
+
+# Delete poll
+@app.delete("/polls/{poll_id}")
+def delete_poll(poll_id: str):
+	polls.pop(poll_id)
+	votes.pop(poll_id)
+	return {"ok": True}
+
+# Vote
+@app.post("/polls/{poll_id}/vote")
+def cast_vote(poll_id: str, vote: Vote):
+	v = votes[poll_id]
+	options = polls[poll_id]["options"]
+	if vote.option not in options:
+		return {"ok": False, "msg": "Invalid option", "available_options": options}
+	v[vote.option] = v.get(vote.option, 0) + 1
+	return {"ok": True}
+
+# Get results
+@app.get("/polls/{poll_id}/results")
+def get_results(poll_id: str):
+	return votes[poll_id]
